cd7f0f4cc5c2b9152469450b03c37fa0c6b94540
Mitsuki Watanabe <ix5231if@gmail.com>
2019-01-18 18:16:41 +0900

    Cat a file from an argument.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use std::{env, process};
+use std::path::Path;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
 
 fn usage() {
     println!("USAGE");
     println!("    fill INPUT");
 }
 
+fn show<P: AsRef<Path>>(path: P) {
+    let input = File::open(path).unwrap();
+    let buf = BufReader::new(&input);
+
+    for l in buf.lines() {
+        println!("{}", l.unwrap());
+    }
+}
+
 fn main() {
     if let Some(file_path) = env::args().nth(1) {
-        println!("{}", file_path);
+        show(file_path);
     } else {
         usage();
         process::exit(1);
