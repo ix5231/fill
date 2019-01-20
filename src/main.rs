ff81443c871e9b788bf4e23c69ca73f47e061996
Mitsuki Watanabe <ix5231if@gmail.com>
2019-01-20 16:53:02 +0900

    Implement fill form
    This is not interactive though.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,54 @@
 use regex::Regex;
+use std::borrow::Cow;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 use std::{env, process};
 
-/// Denotes forms in the target.
-#[derive(Debug, Eq, PartialEq)]
-struct Form<'a> {
-    name: &'a str,
+/// Input text.
+struct Text<'a> {
+    input: Cow<'a, str>,
 }
 
-impl<'a> Form<'a> {
-    fn new(name: &str) -> Form {
-        Form { name }
+impl<'a> Text<'a> {
+    // Create `Text`.
+    fn new(input: &str) -> Text {
+        Text {
+            input: Cow::from(input),
+        }
+    }
+
+    /// Get forms from the input.
+    /// Returns empty vector if the input does not contain forms.
+    fn get_forms(&self) -> Vec<Form> {
+        let re = Regex::new(r"\{(.*?)\}").unwrap();
+        let mut result = vec![];
+
+        for name in re.captures_iter(&self.input) {
+            result.push(Form::new(
+                name.get(1).unwrap().as_str().to_string(),
+                name.get(0).unwrap().as_str().to_string(),
+            ));
+        }
+
+        result
+    }
+
+    fn fill_form(&mut self, form: &Form, replace_text: &str) {
+        self.input = Cow::from(self.input.replace(&form.form, replace_text));
+    }
+}
+
+/// Forms in the target.
+#[derive(Debug, Eq, PartialEq)]
+struct Form {
+    name: String,
+    form: String,
+}
+
+impl Form {
+    fn new(name: String, form: String) -> Form {
+        Form { name, form }
     }
 }
 
@@ -42,19 +78,6 @@ fn main() {
     process::exit(0);
 }
 
-/// Get forms from the input.
-/// Returns empty vector if the input does not contain forms.
-fn get_forms(input: &str) -> Vec<Form> {
-    let re = Regex::new(r"\{(.*?)\}").unwrap();
-    let mut result = vec![];
-
-    for name in re.captures_iter(input) {
-        result.push(Form::new(name.get(1).unwrap().as_str()));
-    }
-
-    result
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
@@ -63,29 +86,62 @@ mod test {
     fn test_get_forms() {
         // Single form
         assert_eq!(
-            get_forms("Hello, my name is {name}."),
-            vec![Form::new("name")]
+            Text::new("Hello, my name is {name}.").get_forms(),
+            vec![Form::new("name".to_string(), "{name}".to_string())]
         );
 
         // Multiple forms
         assert_eq!(
-            get_forms("Hello, my name is {name}. I'm {age} and live in {place}."),
-            vec![Form::new("name"), Form::new("age"), Form::new("place")]
+            Text::new("Hello, my name is {name}. I'm {age} and live in {place}.").get_forms(),
+            vec![
+                Form::new("name".to_string(), "{name}".to_string()),
+                Form::new("age".to_string(), "{age}".to_string()),
+                Form::new("place".to_string(), "{place}".to_string())
+            ]
         );
 
         // None
-        assert_eq!(
-            get_forms("Hello"),
-            vec![]
-        );
+        assert_eq!(Text::new("Hello").get_forms(), vec![]);
 
         // Multiple lines
         assert_eq!(
-            get_forms(
+            Text::new(
                 "Hello, my name is {name}.
                       I'm {age} and live in {place}."
-            ),
-            vec![Form::new("name"), Form::new("age"), Form::new("place")]
+            )
+            .get_forms(),
+            vec![
+                Form::new("name".to_string(), "{name}".to_string()),
+                Form::new("age".to_string(), "{age}".to_string()),
+                Form::new("place".to_string(), "{place}".to_string())
+            ]
         );
+    }
+
+    #[test]
+    fn test_fill_form() {
+        let mut text = Text::new(
+            "Hello, my name is {name}.
+                      I'm {age} and live in {place}.",
+        );
+        let mut forms = text.get_forms();
+        assert_eq!(
+            forms,
+            vec![
+                Form::new("name".to_string(), "{name}".to_string()),
+                Form::new("age".to_string(), "{age}".to_string()),
+                Form::new("place".to_string(), "{place}".to_string())
+            ]
+        );
+
+        let input = ["Ix5231", "19", "Japan"];
+        for (form, i) in forms.iter_mut().zip(&input) {
+            text.fill_form(form, i);
+        }
+        assert_eq!(
+            text.input,
+            "Hello, my name is Ix5231.
+                      I'm 19 and live in Japan.",
+        )
     }
 }
